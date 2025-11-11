97326aca1d831f292a1ca97533224053ff4c8913
kurapica <kurapica125@outlook.com>
2025-11-11 18:20:49 +0800

    temp

---
+++ src/components/anyView.vue
@@ -0,0 +1,15 @@
+<template>
+    <span></span>
+</template>
+
+<script lang="ts" setup>
+import { ScalarNode } from 'schema-node'
+
+// Define props
+const props = defineProps<{
+    /**
+     * Scalar schema node
+     */
+    node: ScalarNode,
+}>()
+</script>
