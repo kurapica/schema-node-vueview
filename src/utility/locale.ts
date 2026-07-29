29205d1ea7faaa9c5566790b65f294cd71199084
kurapica <kurapica125@outlook.com>
2026-07-29 16:57:26 +0800

    t

--- src/utility/locale.ts
+++ src/utility/locale.ts
@@ -1,6 +1,7 @@
 import { subscribeLanguage, type LocaleFunction, getLocaleProxy } from "schema-node-core"
 import { ref } from "vue"
 
+/** reactive locale function */
 export const _L = ref<LocaleFunction>(getLocaleProxy());
 
 // force reload to force re-render
